7bf66e94aeb2dda565f8066c66befbb56e893078
Luis Tanese <luistanese@gmail.com>
2025-09-10 08:37:01 -0300

    fix headers

--- src/server.js
+++ src/server.js
@@ -17,6 +17,8 @@ validateEnvironment();
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
