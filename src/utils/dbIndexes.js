06873a8a023d629a424ac2f7de4955a10cda37d1
Luis Tanese <luistanese@gmail.com>
2025-09-18 20:06:48 -0300

    aa

--- src/utils/dbIndexes.js
+++ src/utils/dbIndexes.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2025 Tanese. All Rights Reserved.
  *
  * This software is proprietary and confidential. Unauthorized copying, distribution,
- * modification, or use of this software is strictly prohibited.
+ * modification, or use of this software is strictly prohibited. 
  */
 
 const { getUsersCollection, getReviewsCollection } = require("./database");
