4d65638afe6f4a60e7ed121ba24802ee0a592e8d
SillyestWomperson <luis_tanese@estudante.sesisenai.org.br>
2025-09-18 11:10:54 -0300

    Fix nome de trilhas

--- src/models/review.js
+++ src/models/review.js
@@ -10,200 +10,229 @@ const { getReviewsCollection } = require("../utils/database");
 const User = require("./user");
 
 class Review {
-	constructor(reviewData) {
-		this.trailId = reviewData.trailId;
-		this.trailName = reviewData.trailName;
-		this.userId = reviewData.userId;
-		this.username = reviewData.username;
-		this.userProfilePic = reviewData.userProfilePic;
-		this.rating = reviewData.rating;
-		this.review = reviewData.review;
-		this.createdAt = reviewData.createdAt || new Date();
-		this.updatedAt = reviewData.updatedAt || new Date();
-	}
+    constructor(reviewData) {
+        this.trailId = reviewData.trailId;
+        this.trailName = reviewData.trailName;
+        this.userId = reviewData.userId;
+        this.username = reviewData.username;
+        this.userProfilePic = reviewData.userProfilePic;
+        this.rating = reviewData.rating;
+        this.review = reviewData.review;
+        this.createdAt = reviewData.createdAt || new Date();
+        this.updatedAt = reviewData.updatedAt || new Date();
+    }
 
-	static async syncUserData(review) {
-		try {
-			const currentUser = await User.findById(review.userId);
+    static async syncUserData(review) {
+        try {
+            const currentUser = await User.findById(review.userId);
 
-			if (currentUser) {
-				return {
-					...review,
-					username: currentUser.username,
-					userProfilePic: currentUser.profilePicUrl,
-				};
-			} else {
-				return {
-					...review,
-					username: "Usuário Deletado",
-					userProfilePic: "",
-				};
-			}
-		} catch (error) {
-			return {
-				...review,
-				username: "Usuário Deletado",
-				userProfilePic: "",
-			};
-		}
-	}
+            if (currentUser) {
+                return {
+                    ...review,
+                    username: currentUser.username,
+                    userProfilePic: currentUser.profilePicUrl,
+                };
+            } else {
+                return {
+                    ...review,
+                    username: "Usuário Deletado",
+                    userProfilePic: "",
+                };
+            }
+        } catch (error) {
+            return {
+                ...review,
+                username: "Usuário Deletado",
+                userProfilePic: "",
+            };
+        }
+    }
 
-	static async syncMultipleReviews(reviews) {
-		return await Promise.all(reviews.map((review) => Review.syncUserData(review)));
-	}
+    static async syncMultipleReviews(reviews) {
+        return await Promise.all(
+            reviews.map((review) => Review.syncUserData(review))
+        );
+    }
 
-	static async create(reviewData, userId) {
-		const reviewsCollection = await getReviewsCollection();
-		const Trail = require("./trail");
+    static async create(reviewData, userId) {
+        const reviewsCollection = await getReviewsCollection();
+        const Trail = require("./trail");
 
-		if (!Trail.exists(reviewData.trailId)) {
-			throw new Error("TRAIL_NOT_FOUND");
-		}
+        if (!Trail.exists(reviewData.trailId)) {
+            throw new Error("TRAIL_NOT_FOUND");
+        }
 
-		const existingReview = await reviewsCollection.findOne({
-			trailId: reviewData.trailId,
-			userId: new ObjectId(userId),
-		});
+        const actualTrail = Trail.findById(reviewData.trailId);
+        if (!actualTrail) {
+            throw new Error("TRAIL_NOT_FOUND");
+        }
 
-		if (existingReview) {
-			throw new Error("ALREADY_REVIEWED");
-		}
+        const correctedReviewData = {
+            ...reviewData,
+            trailName: actualTrail.name,
+        };
 
-		const user = await User.findById(userId);
-		if (!user) {
-			throw new Error("USER_NOT_FOUND");
-		}
+        const existingReview = await reviewsCollection.findOne({
+            trailId: reviewData.trailId,
+            userId: new ObjectId(userId),
+        });
 
-		const review = new Review({
-			...reviewData,
-			userId: new ObjectId(userId),
-			username: user.username,
-			userProfilePic: user.profilePicUrl,
-		});
+        if (existingReview) {
+            throw new Error("ALREADY_REVIEWED");
+        }
 
-		const result = await reviewsCollection.insertOne(review);
-		return { ...review, _id: result.insertedId };
-	}
+        const user = await User.findById(userId);
+        if (!user) {
+            throw new Error("USER_NOT_FOUND");
+        }
 
-	static async findById(reviewId) {
-		const reviewsCollection = await getReviewsCollection();
-		const review = await reviewsCollection.findOne({ _id: new ObjectId(reviewId) });
+        const review = new Review({
+            ...correctedReviewData,
+            userId: new ObjectId(userId),
+            username: user.username,
+            userProfilePic: user.profilePicUrl,
+        });
 
-		if (!review) {
-			return null;
-		}
+        const result = await reviewsCollection.insertOne(review);
+        return { ...review, _id: result.insertedId };
+    }
 
-		return await Review.syncUserData(review);
-	}
+    static async findById(reviewId) {
+        const reviewsCollection = await getReviewsCollection();
+        const review = await reviewsCollection.findOne({
+            _id: new ObjectId(reviewId),
+        });
 
-	static async update(reviewId, updateData, userId) {
-		const reviewsCollection = await getReviewsCollection();
+        if (!review) {
+            return null;
+        }
 
-		const review = await reviewsCollection.findOne({ _id: new ObjectId(reviewId) });
-		if (!review) {
-			throw new Error("REVIEW_NOT_FOUND");
-		}
+        return await Review.syncUserData(review);
+    }
 
-		if (!review.userId.equals(new ObjectId(userId))) {
-			throw new Error("CANNOT_EDIT_REVIEW");
-		}
+    static async update(reviewId, updateData, userId) {
+        const reviewsCollection = await getReviewsCollection();
 
-		await reviewsCollection.updateOne(
-			{ _id: new ObjectId(reviewId) },
-			{
-				$set: {
-					...updateData,
-					updatedAt: new Date(),
-				},
-			}
-		);
+        const review = await reviewsCollection.findOne({
+            _id: new ObjectId(reviewId),
+        });
+        if (!review) {
+            throw new Error("REVIEW_NOT_FOUND");
+        }
 
-		const updatedReview = await reviewsCollection.findOne({ _id: new ObjectId(reviewId) });
-		return await Review.syncUserData(updatedReview);
-	}
+        if (!review.userId.equals(new ObjectId(userId))) {
+            throw new Error("CANNOT_EDIT_REVIEW");
+        }
 
-	static async delete(reviewId, userId) {
-		const reviewsCollection = await getReviewsCollection();
+        await reviewsCollection.updateOne(
+            { _id: new ObjectId(reviewId) },
+            {
+                $set: {
+                    ...updateData,
+                    updatedAt: new Date(),
+                },
+            }
+        );
 
-		const review = await reviewsCollection.findOne({ _id: new ObjectId(reviewId) });
-		if (!review) {
-			throw new Error("REVIEW_NOT_FOUND");
-		}
+        const updatedReview = await reviewsCollection.findOne({
+            _id: new ObjectId(reviewId),
+        });
+        return await Review.syncUserData(updatedReview);
+    }
 
-		if (!review.userId.equals(new ObjectId(userId))) {
-			throw new Error("CANNOT_DELETE_REVIEW");
-		}
+    static async delete(reviewId, userId) {
+        const reviewsCollection = await getReviewsCollection();
 
-		await reviewsCollection.deleteOne({ _id: new ObjectId(reviewId) });
-		return true;
-	}
+        const review = await reviewsCollection.findOne({
+            _id: new ObjectId(reviewId),
+        });
+        if (!review) {
+            throw new Error("REVIEW_NOT_FOUND");
+        }
 
-	static async getByUserId(userId) {
-		const reviewsCollection = await getReviewsCollection();
+        if (!review.userId.equals(new ObjectId(userId))) {
+            throw new Error("CANNOT_DELETE_REVIEW");
+        }
 
-		const reviews = await reviewsCollection
-			.find({ userId: new ObjectId(userId) })
-			.sort({ createdAt: -1 })
-			.toArray();
+        await reviewsCollection.deleteOne({ _id: new ObjectId(reviewId) });
+        return true;
+    }
 
-		return await Review.syncMultipleReviews(reviews);
-	}
+    static async getByUserId(userId) {
+        const reviewsCollection = await getReviewsCollection();
 
-	static async getByTrailId(trailId) {
-		const reviewsCollection = await getReviewsCollection();
+        const reviews = await reviewsCollection
+            .find({ userId: new ObjectId(userId) })
+            .sort({ createdAt: -1 })
+            .toArray();
 
-		const reviews = await reviewsCollection.find({ trailId }).sort({ createdAt: -1 }).toArray();
+        return await Review.syncMultipleReviews(reviews);
+    }
 
-		return await Review.syncMultipleReviews(reviews);
-	}
+    static async getByTrailId(trailId) {
+        const reviewsCollection = await getReviewsCollection();
 
-	static async getAll() {
-		const reviewsCollection = await getReviewsCollection();
-		const reviews = await reviewsCollection.find({}).sort({ createdAt: -1 }).toArray();
+        const reviews = await reviewsCollection
+            .find({ trailId })
+            .sort({ createdAt: -1 })
+            .toArray();
 
-		return await Review.syncMultipleReviews(reviews);
-	}
+        return await Review.syncMultipleReviews(reviews);
+    }
 
-	static async hasUserReviewed(userId, trailId) {
-		const reviewsCollection = await getReviewsCollection();
+    static async getAll() {
+        const reviewsCollection = await getReviewsCollection();
+        const reviews = await reviewsCollection
+            .find({})
+            .sort({ createdAt: -1 })
+            .toArray();
 
-		const review = await reviewsCollection.findOne({
-			userId: new ObjectId(userId),
-			trailId: trailId,
-		});
+        return await Review.syncMultipleReviews(reviews);
+    }
 
-		return !!review;
-	}
+    static async hasUserReviewed(userId, trailId) {
+        const reviewsCollection = await getReviewsCollection();
 
-	static async getTrailStats(trailId) {
-		const reviewsCollection = await getReviewsCollection();
+        const review = await reviewsCollection.findOne({
+            userId: new ObjectId(userId),
+            trailId: trailId,
+        });
 
-		const reviews = await reviewsCollection.find({ trailId }).toArray();
+        return !!review;
+    }
 
-		if (reviews.length === 0) {
-			return {
-				totalReviews: 0,
-				averageRating: 0,
-				ratingDistribution: {},
-			};
-		}
+    static async getTrailStats(trailId) {
+        const reviewsCollection = await getReviewsCollection();
 
-		const totalReviews = reviews.length;
-		const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
-		const averageRating = Math.round((totalRating / totalReviews) * 2) / 2;
+        const reviews = await reviewsCollection.find({ trailId }).toArray();
 
-		const ratingDistribution = {};
-		reviews.forEach((review) => {
-			const rating = review.rating;
-			ratingDistribution[rating] = (ratingDistribution[rating] || 0) + 1;
-		});
+        if (reviews.length === 0) {
+            return {
+                totalReviews: 0,
+                averageRating: 0,
+                ratingDistribution: {},
+            };
+        }
 
-		return {
-			totalReviews,
-			averageRating,
-			ratingDistribution,
-		};
-	}
+        const totalReviews = reviews.length;
+        const totalRating = reviews.reduce(
+            (sum, review) => sum + review.rating,
+            0
+        );
+        const averageRating = Math.round((totalRating / totalReviews) * 2) / 2;
+
+        const ratingDistribution = {};
+        reviews.forEach((review) => {
+            const rating = review.rating;
+            ratingDistribution[rating] = (ratingDistribution[rating] || 0) + 1;
+        });
+
+        return {
+            totalReviews,
+            averageRating,
+            ratingDistribution,
+        };
+    }
 }
 
 module.exports = Review;
